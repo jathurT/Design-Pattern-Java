f132808f22cc45499f61fadc3750506c6ae4ad4a
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-11-13 13:45:21 +0530

    MVC Pattern Updated

--- src/main/java/com/uor/architectural/mvc/Client.java
+++ src/main/java/com/uor/architectural/mvc/Client.java
@@ -4,12 +4,8 @@ public class Client {
   public static void main(String[] args) {
     // Fetch student record based on his roll no from the database
     Student model = retrieveStudentFromDatabase();
-
-    // Create a view : to write student details on console
     StudentView view = new StudentView();
-
     StudentController controller = new StudentController(model, view);
-
     controller.updateView();
 
     // Update model data
