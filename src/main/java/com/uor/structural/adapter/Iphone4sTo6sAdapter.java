b7c23159c1f183d3179f2badc8f5aedcf0ee741d
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-11-14 08:21:04 +0530

    Enhance the code

--- src/main/java/com/uor/structural/adapter/Iphone4sTo6sAdapter.java
+++ src/main/java/com/uor/structural/adapter/Iphone4sTo6sAdapter.java
@@ -3,7 +3,7 @@ package com.uor.structural.adapter;
 //Adapter class
 public class Iphone4sTo6sAdapter implements Charger {
 
-  IPhone4sCharger iPhone4sCharger;
+  private final IPhone4sCharger iPhone4sCharger;
 
   public Iphone4sTo6sAdapter(IPhone4sCharger iPhone4sCharger) {
     this.iPhone4sCharger = iPhone4sCharger;
