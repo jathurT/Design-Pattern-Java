c7af846a3687d2050165bd8c6a5e77e7b8fec046
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-11-18 17:10:33 +0530

    Enhanced the code

--- src/main/java/com/uor/behavioral/template/CoffeeMaker.java
+++ src/main/java/com/uor/behavioral/template/CoffeeMaker.java
@@ -2,6 +2,12 @@ package com.uor.behavioral.template;
 
 // Concrete Class
 public class CoffeeMaker extends BeverageMaker {
+
+  @Override
+  public void boilWater() {
+    System.out.println("Boil water for extra two minutes");
+  }
+
   @Override
   public void brew() {
     System.out.println("Dripping Coffee through filter");
